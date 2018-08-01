cca55143da117cfcb884d0ca0571afaa3edd78ce
xiecheng328 <13136666328@163.com>
2018-08-01 11:57:43 +0800

    rwd

--- 移动端web开发/starbucks/js/index.js
+++ 移动端web开发/starbucks/js/index.js
@@ -10,29 +10,46 @@ $(function(){
     });
 
     // 轮播图
-    var w = $('.slide-container img').width();
+    var clientW = document.body.clientWidth;
     $('.slide-container').css('width', function(){
-        return 4 * w;
+        return 4 * clientW;
     });
+    window.onresize = function(){
+        clientW = document.body.clientWidth;
+        $('.slide-container img').css({
+            width: clientW
+        });
+        $('.slide-container').css('width', function(){
+            return 4 * clientW;
+        });
+    };
+
 
     var nowIndex = 1;
 
-    setInterval(function(){
+    var speed = 1500;
+
+    var play = function(){
         if(nowIndex == 4){
             nowIndex = 1;
             $('.slide-container').css({
                 transition: 'none',
                 transform: 'translateX(0px)'
             });
+            speed = 0;
         }else{
             $('.slide-container').css({
                 transition: 'transform .8s ease',
-                transform: 'translateX(-'+ w * (nowIndex) +'px)'
+                transform: 'translateX(-'+ clientW * (nowIndex++) +'px)'
             });
+            speed = 1500;
         }
-        nowIndex++;
+        clearTimeout(timer);
+        timer = setTimeout(play, speed);
+    };
+    var timer = setTimeout(play, speed);
 
-    }, 1500);
+
 
 
 });
