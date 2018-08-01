aeeea7a8de249a3dcbad98d29c4c5685b4024014
xiecheng328 <13136666328@163.com>
2018-08-01 17:06:31 +0800

    rwd

--- 移动端web开发/starbucks/js/index.js
+++ 移动端web开发/starbucks/js/index.js
@@ -1,27 +1,36 @@
-$(function(){
-    $('.nav-menu img').on('click', function(){
+$(function () {
+    $('.nav-menu img').on('click', function () {
         $('.overlay').addClass('actived');
         return false;
     });
 
-    $('.nav-close img').on('click', function(){
-       $('.overlay').removeClass('actived');
-       return false;
+    $('.nav-close img').on('click', function () {
+        $('.overlay').removeClass('actived');
+        return false;
     });
 
     // 轮播图
-    var clientW = document.body.clientWidth;
-    $('.slide-container').css('width', function(){
+    var $Slide = $('.slide');
+    var clientW = $Slide.width();
+    $('.slide-container').css('width', function () {
         return 4 * clientW;
     });
-    window.onresize = function(){
-        clientW = document.body.clientWidth;
-        $('.slide-container img').css({
-            width: clientW
-        });
-        $('.slide-container').css('width', function(){
-            return 4 * clientW;
-        });
+    $('.slide-container .slide-item').css({
+        width: clientW
+    });
+    var t;
+    window.onresize = function () {
+        clearTimeout(t);
+        t = setTimeout(function () {
+            clientW = $Slide.width();
+            $('.slide-container').css('width', function () {
+                return 4 * clientW;
+            });
+            $('.slide-container .slide-item').css({
+                width: clientW
+            });
+        }, 17);
+
     };
 
 
@@ -29,18 +38,18 @@ $(function(){
 
     var speed = 1500;
 
-    var play = function(){
-        if(nowIndex == 4){
+    var play = function () {
+        if (nowIndex == 4) {
             nowIndex = 1;
             $('.slide-container').css({
                 transition: 'none',
                 transform: 'translateX(0px)'
             });
             speed = 0;
-        }else{
+        } else {
             $('.slide-container').css({
                 transition: 'transform .8s ease',
-                transform: 'translateX(-'+ clientW * (nowIndex++) +'px)'
+                transform: 'translateX(-' + clientW * (nowIndex++) + 'px)'
             });
             speed = 1500;
         }
@@ -48,8 +57,6 @@ $(function(){
         timer = setTimeout(play, speed);
     };
     var timer = setTimeout(play, speed);
-
-
 
 
 });
