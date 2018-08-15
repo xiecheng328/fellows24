a14cd0a290a27626584f2d92905eff9ca241f7ad
xiecheng328 <13136666328@163.com>
2018-08-15 10:33:19 +0800

    vueapp

---
+++ vueapp/src/components/Player.vue
@@ -0,0 +1,136 @@
+<template>
+    <div>
+        <!-- 专辑信息 -->
+        <div class="album">
+            <div class="album-mask" :style="{background: 'url('+albumImg+') no-repeat center/cover'}"></div>
+            <div class="album-img">
+                <img :src="albumImg" alt="">
+            </div>
+            <div class="album-info">
+                <p class="album-info-title">{{albumTitle}}</p>
+                <p class="album-info-author">{{albumAuthor}}</p>
+                <div class="album-info-control clearfix">
+                    <div class="album-info-control-icon">
+                        <i class="icon iconfont icon-shangyishou"></i>
+                        <i class="icon iconfont icon-bofang" v-show="!isPlay"></i>
+                        <i class="icon iconfont icon-zanting" v-show="isPlay"></i>
+                        <i class="icon iconfont icon-xiayishou"></i>
+                    </div>
+                    <span class="album-info-control-menu">歌单</span>
+                </div>
+            </div> 
+        </div>
+
+        <!-- 歌单 -->
+        <ul class="music-list">
+            <li @click="selectMusic(music, index)" :class="['music-list-item', nowIndex == index?'selected': '']" v-for="(music,index) in musicList" :key="index">
+                <span class="music-list-item-title">{{music.title}}&nbsp;-&nbsp;</span>
+                <span class="music-list-item-author">{{music.author}}</span>
+            </li>
+        </ul>
+
+    </div>
+
+
+   
+</template>
+
+<script>
+import "@/assets/font/iconfont.css";
+export default {
+  props: ["musicList"],
+  data() {
+    return {
+      nowIndex: -1, // 当前选中歌曲的索引
+      albumImg:
+        "http://img3.imgtn.bdimg.com/it/u=1039246244,1205520727&fm=27&gp=0.jpg", //专辑的封面
+      albumTitle: "", //歌曲名称
+      albumAuthor: "", //歌曲歌手
+      isPlay: false // 是否在播放
+    };
+  },
+  methods: {
+    selectMusic(music, index) {
+      this.nowIndex = index;
+      this.albumImg = music.musicImgSrc;
+      this.albumTitle = music.title;
+      this.albumAuthor = music.author;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.clearfix::after {
+  content: "";
+  display: block;
+  clear: both;
+}
+.music-list {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background-color: #2a2929;
+  height: 4rem;
+  overflow-y: scroll;
+  &-item {
+    color: #dcdbdb;
+    border-bottom: 0.02rem solid #343433;
+    padding: 0.2rem;
+
+    &.selected {
+      color: #299557;
+    }
+  }
+}
+
+.album {
+  display: flex;
+  text-align: center;
+  position: relative;
+  color: #fff;
+  &-mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    filter: blur(20px);
+    z-index: -1;
+  }
+
+  &-img {
+    flex-grow: 1;
+    width: 0;
+    margin-left: 0.2rem;
+    img {
+      width: 100%;
+    }
+  }
+  &-info {
+    width: 0;
+    flex-grow: 2;
+    &-title {
+      font-size: 0.5rem;
+    }
+    &-control {
+      position: relative;
+      height: 0.9rem;
+      line-height: 0.9rem;
+      &-icon {
+        float: left;
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        .icon {
+          font-size: 0.5rem;
+        }
+      }
+      &-menu {
+        float: right;
+        margin-right: 0.4rem;
+      }
+    }
+  }
+}
+</style>
--- vueapp/src/views/music/Music.vue
+++ vueapp/src/views/music/Music.vue
@@ -1,13 +1,26 @@
 <template>
-    <h1>music</h1>
+    <player :musicList="musicList"></player>
 </template>
 
 
 <script>
+import Player from '@/components/Player.vue';
+import axios from  'axios';
 export default {
+    data(){
+        return {
+            musicList: []
+        };
+    },
     created(){
         // console.log('music');
         this.$emit('switchTab', 'music');
+        axios.get('/data/musicdata.json').then(res=>{
+            this.musicList = res.data.musicData;
+        });
+    },
+    components: {
+        Player
     }
 }
 </script>
