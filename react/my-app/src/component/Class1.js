f7a7f1d4f893a304c1159c939d0f5a5b0386188f
fdf_100000581038_fengchuanyu <fdf_100000581038_fengchuanyu@git.cloud.tencent.com>
2018-09-15 16:58:45 +0800

    admin

--- react/my-app/src/component/Class1.js
+++ react/my-app/src/component/Class1.js
@@ -4,18 +4,20 @@ import {
   Route, 
   Link ,
   Switch,
-  Redirect
+  Redirect,
+  withRouter,
+  Prompt,
+  NavLink
  } from "react-router-dom";
 const Home = ()=><h3>home</h3>
 const About = ({match})=>{
-  console.log(match);
   return <h3>aubout{match.params.id}</h3>
 }
 const MenuLink = ({to,lable})=>{
   return(
     <Route 
       path={to}
-      children={({match})=>{
+      children={()=>{
        return(
         <div>
           <Link to={to}>{lable}</Link>
@@ -25,14 +27,33 @@ const MenuLink = ({to,lable})=>{
     />
   )
 }
+const RouteChange = withRouter(({history})=>{
+  return(
+    <div>
+      <button onClick={()=>{history.push('/class1/')}}>首页</button>
+    </div>
+  )
+})
 class Class1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      isPrompt:false
+    }
+  }
+  
   render() {
     return (
       <Router>
       <div>
-        <Link to="/class1/">home</Link>|
-        <Link to="/class1/about/111">about</Link>|
-        <Link to="/class1/redirect">redirect</Link>
+        <Prompt
+          when={this.state.isPrompt}
+          message="确认跳转？"
+        />
+        <RouteChange/>
+        <NavLink isActive={()=>false} activeClassName="aaa" exact to="/class1/">home</NavLink>|
+        <NavLink activeStyle={{color:'red'}} activeClassName="aaa" to="/class1/about/111">about</NavLink>|
+        <NavLink activeClassName="aaa" to="/class1/redirect">redirect</NavLink>
         <MenuLink to="/class1/" lable="menulink"/>
         <Route exact path="/class1/" render={()=><h3>renderhome</h3>}/>
         <Route path="/class1/about/:id" component={About}/>
